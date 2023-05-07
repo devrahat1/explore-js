f6b88639b5c618e8f85025be4816c3951044ba50
Dev.Rahat Hasan <devrahat.hasan@gmail.com>
2023-05-08 01:15:43 +0600

    exploring javascript

---
+++ duplicate.js
@@ -0,0 +1,19 @@
+const names = ["abul", "babul", "cabul", "dabul", "evul", "rahat", "maruf", "oli", "abul", "babul", "rahat", 'maruf'];
+
+function removeDuplicate(names) {
+    const unique = []
+
+
+    for (let i = 0; i < names.length; i++) {
+        const name = names[i];
+
+
+        if (unique.includes(name) === false) {
+            unique.push(name);
+
+        }
+    }
+    return unique;
+}
+const uniqueNames = removeDuplicate(names);
+console.log (uniqueNames);
